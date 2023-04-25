fc8a7776a87abbf43420b806848028197397a1cd
evening8 <ishimwerichard26@gmail.com>
2023-04-25 13:42:43 +0200

    grids

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,28 @@
-let num1 = 4;
-num1 = 6;
-console.log(num1);
+// string concantinating 
+// variable declaration
+// let , const //data
+// if statement
+/*
+if(codition){
+
+}else{
+
+}
+*/
+
+
+let firstName = "munezero";
+let secondName = "Richard";
+if(firstName === "munezero"){
+ console.log("correct!!!!!");
+}else{
+    console.log("invalid first name")
+}
+
+
+// console.log("first person:" + firstName +" "+ secondName);
+// firstName = "nyiransengimana";
+// secondName = "edith";
+// console.log("second person:" + firstName + " " + secondName);
+// console.log(2 + 2);
+// console.log("4");
