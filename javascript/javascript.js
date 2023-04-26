00963f9928c4881a9626ed1162b3ce4436075556
evening8 <ishimwerichard26@gmail.com>
2023-04-26 20:13:24 +0200

    using function in js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,28 +1,139 @@
-// string concantinating 
-// variable declaration
-// let , const //data
-// if statement
+
 /*
-if(codition){
+condotional statements
+if
+and &&  or ||
+array: 
+let arrayName = [item1, item2, item3 ....itemn]
+[0,1,2,3,4,5,6...............n]
+1.push: adding item on the array
+2.pop: deleting item
 
-}else{
-
-}
+find if the sum of two numbers in array of studentsAge can 
+be an index of an item in the array of students
+else console.log("unauthorized")
 */
 
+// let , const
+// let email = "laptop@gmail.com";
+// let firstName = "nyiransengimana";
+// let a = 7;
+// let b = 0;
+// let emptyArray = [];
+// let students = ["ndizihiwe", "kanyana", "nyiransengimana", "munezero", "ishimwe"];
+// let studentsAge = [1,2,3,4,5,3,6];
+// let sum = studentsAge[a] + studentsAge[b];
+// console.log("sum is:", sum);
+// if(sum <  students.length){
+//     console.log("student is:", students[sum]);
+// }else{
+//     console.log(`student does not exist at index ${sum}`)
+// }
+// [0, 1, 2, 3]
+// let num1 = 4;
+// let num2 = "4"
+// console.log(num2);
+// console.log(num1);
+// consol
+// emptyArray.push("innocent");
+// console.log("emaptyArray with innocent:", emptyArray);
+// emptyArray.push("claude");
+// console.log("emptyArray with claude:", emptyArray);
+// emptyArray.pop();
+// console.log("empty array claude deleted:", emptyArray)
+// console.log("array of numbers", studentsAge);
+// let sum = studentsAge[0] + studentsAge[1];
+// console.log("length of the array:", students.length)
+// console.log("sum:", sum);
+// if(email === "changed@gmail.com" || firstName === "kanyana"){
+//     console.log("you are welcome admin dashboard")
+    
+// }else{
+//    console.log("authentication failed")
+// }
 
-let firstName = "munezero";
-let secondName = "Richard";
-if(firstName === "munezero"){
- console.log("correct!!!!!");
-}else{
-    console.log("invalid first name")
+
+
+// window.alert("this is an alert");
+let numArray = ["zero","one", "two", "three"];
+// fnctions: combination of reusable lines of codes
+let number = 3;
+// number[]
+// if(number <= 10){
+//     numArray.push("four");
+//     console.log(numArray);
+// }else{
+//     numArray.pop();
+//     console.log(numArray);
+// }
+// let functionName = () =>{
+// }
+// let edith = () =>{
+    // console.log("i am in function...........");
+// }
+// calling a function
+// functionName();
+// edith();
+
+
+let senior=()=>{
+    console.log("allowed to have beer");
 }
+let junior=()=>{
+        console.log("not allowed to have beer");
+    }
+let person=8;
+// parameter
 
 
-// console.log("first person:" + firstName +" "+ secondName);
-// firstName = "nyiransengimana";
-// secondName = "edith";
-// console.log("second person:" + firstName + " " + secondName);
-// console.log(2 + 2);
-// console.log("4");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let checkAge = (name) =>{
+//     if(name === "green"){
+//         console.log("go")
+//     }else if(name === "yellow"){
+//         console.log("attention")
+//     }else if(name === "red"){
+// console.log("stop")
+//     }else{
+//         console.log("arrested")
+//     }
+// }
+
+// checkAge("yellow");
+
+
+
+
+
+
+
+
+let checkAge=(age)=>{
+    if(age>=18){
+        console.log("allowed to take beer");
+    }
+    else if(age<0){
+        console.log("wrong input");
+    }
+    else if(age<18){
+        console.log("not allowed to take beer");
+    }
+   
+}
+checkAge(-3);
